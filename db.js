d5f57a25e6c7a6a497eb37469cfec05ab9527395
Umesh <mathulkar98@gmail.com>
2023-03-02 17:30:32 +0530

    cloud

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/testing');
+mongoose.connect('mongodb+srv://test:iOOoxoWbkV0up8vE@cluster0.elbrrf2.mongodb.net/?retryWrites=true&w=majority');
